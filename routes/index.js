22025ad685af67fc6c251eb27c6991439850e2f0
Adepiper <52428747+Adepiper@users.noreply.github.com>
2019-10-11 17:21:30 +0100

    changes

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,12 @@ const {ensureAuthenticated} = require('../config/auth')
 router.get('/', (req, res) => res.render('welcome'))
 
 //dashboard
-router.get('/intern/dashboard',  (req, res)=> res.render('internDashboard'))
+router.get('/intern/dashboard', ensureAuthenticated, (req, res)=> res.render('internDashboard', {
+    name: req.user.userName
+}))
 router.get('/mentor/dashboard', (req, res) => res.render('mentorDashboard'))
 router.get('/about', (req, res) => res.render('about'))
-router.get('/classroom',  (req, res)=> res.render('classroom', {
+router.get('/classroom', ensureAuthenticated, (req, res)=> res.render('classroom', {
     name: req.user.userName}))
 router.get('/complaintForm', (req, res) => res.render('complaint-form'))
 router.get('/faq', (req, res) => res.render('faq'))
